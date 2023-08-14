a91156e6e2e3c8f76cf9ea7a055de8c9701ba649
Praveen Rajendran <praveenraja9333@gmail.com>
2023-08-14 12:10:59 +0100

    InitialCommit_14_08_2023

---
+++ src/main/java/com/vrp/system/paymentsystem/paymentledgerservice/PaymentledgerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vrp.system.paymentsystem.paymentledgerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentledgerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentledgerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrp/system/paymentsystem/paymentledgerservice/PaymentledgerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrp.system.paymentsystem.paymentledgerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentledgerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
